e61da778bea0874f794c7591c45a819227789d00
Vikram Dighe <vikram@swiftsignal.com>
2010-02-13 17:21:01 -0500

    Upgraded some gems. Fixed a problem with IR and Responders by freezing to the right versions of each. Changed some verbiage. Added a prompt to install gems via sudo or not (this will also help increase compatibility with platforms which don't provide sudo.)

--- template.rb
+++ template.rb
@@ -2,28 +2,33 @@
 
 # Application gems
 gem 'will_paginate', :lib => 'will_paginate', :source => 'http://gemcutter.org'
-gem 'clearance', :lib => 'clearance', :source => 'http://gemcutter.org'
 gem 'formtastic', :lib => 'formtastic', :source => 'http://gemcutter.org'
 gem 'paperclip', :lib => 'paperclip', :source => 'http://gemcutter.org'
 gem 'compass', :lib => 'compass', :source => 'http://gemcutter.org'
-gem 'inherited_resources', :lib => 'inherited_resources', :source => 'http://gemcutter.org'
 gem 'enum_field', :lib => 'enum_field', :source => 'http://gemcutter.org'
 gem 'dry_scaffold', :lib => false, :source => 'http://gemcutter.org'
+gem 'clearance', :lib => 'clearance', :source => 'http://gemcutter.org', :version => '0.8.5'
+gem 'inherited_resources', :lib => 'inherited_resources', :source => 'http://gemcutter.org', :version => '1.0.3'
+gem 'responders', :source => 'http://gemcutter.org', :version => '0.4.3'
 
 # Testing gems
-gem "cucumber", :lib => false, :version => ">= 0.3.98", :source => 'http://gemcutter.org'
-gem "webrat", :lib => false, :version => ">= 0.4.4", :source => 'http://gemcutter.org'
-gem 'shoulda', :lib => 'shoulda', :version => '>= 2.10.2', :source => 'http://gemcutter.org'
-gem 'factory_girl', :lib => 'factory_girl', :version => '>= 1.2.2', :source => 'http://gemcutter.org'
-gem 'rr', :version => '>= 0.10.0', :source => 'http://gemcutter.org'
-gem 'faker', :version => '>= ', :source => 'http://gemcutter.org'
-gem 'jnunemaker-matchy', :lib => false, :version => '>= 0.4.0', :source => 'http://gems.github.com'
+gem "cucumber", :lib => false, :version => ">= 0.3.98", :source => 'http://gemcutter.org', :env => :test
+gem "webrat", :lib => false, :version => ">= 0.4.4", :source => 'http://gemcutter.org', :env => :test
+gem 'shoulda', :lib => 'shoulda', :version => '>= 2.10.2', :source => 'http://gemcutter.org', :env => :test
+gem 'factory_girl', :lib => 'factory_girl', :version => '>= 1.2.2', :source => 'http://gemcutter.org', :env => :test
+gem 'rr', :version => '>= 0.10.0', :source => 'http://gemcutter.org', :env => :test
+gem 'faker', :version => '>= ', :source => 'http://gemcutter.org', :env => :test
+gem 'jnunemaker-matchy', :lib => false, :version => '>= 0.4.0', :source => 'http://gemcutter.org', :env => :test
 
 # Make sure the gems are installed
-rake("gems:install", :sudo => true)
+if yes?("Install gems with sudo?")
+  rake "gems:install", :sudo => true
+else
+  rake "gems:install"
+end
 
 # Prompt to unpack these gems
-rake("gems:unpack") if yes?("Unpack rails gems ?")
+rake("gems:unpack") if yes?("Unpack rails gems?")
 
 # Create a default controller
 file 'app/controllers/home_controller.rb', 'class HomeController < ApplicationController
